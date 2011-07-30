6ca15c5bb90ac29f07718f8a54590a24317b2b21
Jason Yip <j.c.yip@computer.org>
2011-07-30 10:59:09 +1000

    Generated by Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myotheraddress::Application.config.secret_token = '37ecf4e2ddc7eccf1cf1964964f221a73ed5479820f1c293bae6e9efee9e6a3e0278d0e90b06fe7dee236a3dbfdeecf667188ab1e1c452f2739d14bf2c3923c8'
